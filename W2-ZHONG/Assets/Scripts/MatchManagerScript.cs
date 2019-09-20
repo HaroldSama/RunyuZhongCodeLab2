4a364bb4fa12598a03bb624df882a0b37e1fc689
177sma <howerds4my@gmail.com>
2019-09-20 01:56:53 -0400

    Fix the bug that vertical matches are not removed

--- W2-ZHONG/Assets/Scripts/MatchManagerScript.cs
+++ W2-ZHONG/Assets/Scripts/MatchManagerScript.cs
@@ -16,6 +16,11 @@ public class MatchManagerScript : MonoBehaviour {
 				if(x < _gameManager.gridWidth - 2){
 					match = match || GridHasHorizontalMatch(x, y);
 				}
+
+				if (y < _gameManager.gridHeight - 2)
+				{
+					match = match || GridHasVerticalMatch(x, y);
+				}
 			}
 		}
 		
@@ -26,6 +31,22 @@ public class MatchManagerScript : MonoBehaviour {
 		GameObject token1 = _gameManager.gridArray[x + 0, y];
 		GameObject token2 = _gameManager.gridArray[x + 1, y];
 		GameObject token3 = _gameManager.gridArray[x + 2, y];
+		
+		if(token1 != null && token2 != null && token3 != null){
+			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr2 = token2.GetComponent<SpriteRenderer>();
+			SpriteRenderer sr3 = token3.GetComponent<SpriteRenderer>();
+			
+			return (sr1.sprite == sr2.sprite && sr2.sprite == sr3.sprite);
+		} else {
+			return false;
+		}
+	}
+	
+	public bool GridHasVerticalMatch(int x, int y){
+		GameObject token1 = _gameManager.gridArray[x, y + 0];
+		GameObject token2 = _gameManager.gridArray[x, y + 1];
+		GameObject token3 = _gameManager.gridArray[x, y + 2];
 		
 		if(token1 != null && token2 != null && token3 != null){
 			SpriteRenderer sr1 = token1.GetComponent<SpriteRenderer>();
@@ -65,6 +86,34 @@ public class MatchManagerScript : MonoBehaviour {
 		
 		return matchLength;
 	}
+	
+	private int _GetVerticalMatchLength(int x, int y){
+		int matchLength = 1;
+		
+		GameObject first = _gameManager.gridArray[x, y];
+
+		if(first != null){
+			SpriteRenderer sr1 = first.GetComponent<SpriteRenderer>();
+			
+			for(int i = y + 1; i < _gameManager.gridHeight; i++){
+				GameObject other = _gameManager.gridArray[x, i];
+
+				if(other != null){
+					SpriteRenderer sr2 = other.GetComponent<SpriteRenderer>();
+
+					if(sr1.sprite == sr2.sprite){
+						matchLength++;
+					} else {
+						break;
+					}
+				} else {
+					break;
+				}
+			}
+		}
+		
+		return matchLength;
+	}
 
 	public virtual int RemoveMatches(){
 		int numRemoved = 0;
@@ -82,6 +131,22 @@ public class MatchManagerScript : MonoBehaviour {
 							Destroy(token);
 
 							_gameManager.gridArray[i, y] = null;
+							numRemoved++;
+						}
+					}
+				}
+
+				if (y < _gameManager.gridHeight - 2)
+				{
+					int verticalMatchLength = _GetVerticalMatchLength(x, y);
+					
+					if(verticalMatchLength > 2){
+
+						for(int i = y; i < y + verticalMatchLength; i++){
+							GameObject token = _gameManager.gridArray[x, i]; 
+							Destroy(token);
+
+							_gameManager.gridArray[x, i] = null;
 							numRemoved++;
 						}
 					}
