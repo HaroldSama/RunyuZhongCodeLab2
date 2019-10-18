b517289d83a429bf6a5480c048d73b4734130258
177sma <howerds4my@gmail.com>
2019-10-18 07:32:23 -0400

    AStar - Random Grid

--- W6-ZHONG/Astar/Assets/Scripts/Grids/RandomGridScript.cs
+++ W6-ZHONG/Astar/Assets/Scripts/Grids/RandomGridScript.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class RandomGridScript : GridScript
 {
@@ -14,8 +15,42 @@ public class RandomGridScript : GridScript
 
 	private void Awake()
 	{
-		
+		gridString = new string[gridHeight];
 		// Make the grid be generated into gridString at random w/ the above percentages.
+		for (int i = 0; i < gridHeight; i++)
+		{
+			gridString[i] = RandomizeLine(gridWidth);
+		}
+	}
+	
+	String RandomizeLine(int length)
+	{
+		string line = "";
+
+		for (int i = 0; i < length; i++)
+		{
+			int randomNumber = Random.Range(0, 100);
+
+			if (randomNumber < 20)
+			{
+				line += 'd';
+			}
+			else if (randomNumber >= 20 && randomNumber < 25)
+			{
+				line += 'w';
+			}
+			else if (randomNumber >= 25 && randomNumber < 35)
+			{
+				line += 'r';
+			}
+			else
+			{
+				line += '-';
+			}
+		}
+
+		print(line);
+		return line;
 	}
 
 	protected override Material GetMaterial(int x, int y){
