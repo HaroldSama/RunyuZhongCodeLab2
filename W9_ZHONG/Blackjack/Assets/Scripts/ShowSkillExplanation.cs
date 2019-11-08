4a7881f0212d1afa7c0f8dc07c710ef12c74ab56
177sma <howerds4my@gmail.com>
2019-11-08 12:44:12 -0500

    BlackJack revisit - Skill Board - show explanation on mouse over

---
+++ W9_ZHONG/Blackjack/Assets/Scripts/ShowSkillExplanation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowSkillExplanation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject explanation;
+    
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        explanation.SetActive(true);
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        explanation.SetActive(false);
+    }
+}
